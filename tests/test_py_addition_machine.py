43fa769e978409e02fd2ea8c267c94974060c4b5
Sten Remmelg <sten.remmelg@gmail.com>
2020-12-22 12:56:07 +0200

    add tests for all one bit numbers

--- tests/test_py_addition_machine.py
+++ tests/test_py_addition_machine.py
@@ -1,2 +1,11 @@
-def test_addition():
-    assert 0
+from py_addition_machine.main import add_binary
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "a,b,expected_result", ([0, 0, 0], [1, 0, 1], [0, 1, 1], [1, 1, 10])
+)
+def test_one_bit_numbers(a, b, expected_result):
+    result = add_binary(a, b)
+    assert result == expected_result, f"{result=} != {expected_result=}"
