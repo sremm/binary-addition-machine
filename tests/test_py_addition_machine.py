128b1ae67bf5f725bade2aba58e33ce7a43d2c6c
Sten Remmelg <sten.remmelg@gmail.com>
2020-12-22 13:03:33 +0200

    add some tests for two bit numbers

--- tests/test_py_addition_machine.py
+++ tests/test_py_addition_machine.py
@@ -9,3 +9,11 @@ import pytest
 def test_one_bit_numbers(a, b, expected_result):
     result = add_binary(a, b)
     assert result == expected_result, f"{result=} != {expected_result=}"
+
+
+@pytest.mark.parametrize(
+    "a,b,expected_result", ([10, 1, 11], [10, 10, 100], [10, 11, 101], [11, 11, 110])
+)
+def test_two_bit_numbers(a, b, expected_result):
+    result = add_binary(a, b)
+    assert result == expected_result, f"{result=} != {expected_result=}"
