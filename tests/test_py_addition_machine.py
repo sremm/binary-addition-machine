ce6b911de2f2534841d33e5a6c45453b594238ae
Sten Remmelg <sten.remmelg@gmail.com>
2020-12-22 15:36:34 +0200

    add a test to check that the two implementations return the same results

--- tests/test_py_addition_machine.py
+++ tests/test_py_addition_machine.py
@@ -1,7 +1,14 @@
 from typing import Callable
 from py_addition_machine.main import add_binary_boolean, add_binary_builtin
-
+from hypothesis import given
+import hypothesis.strategies as s
 import pytest
+
+
+@given(s.integers(min_value=0), s.integers(min_value=0))
+def test_binary_and_built_in_implementation_return_same_values(integer1, integer2):
+    binary1, binary2 = bin(integer1)[2:], bin(integer2)[2:]
+    assert add_binary_boolean(binary1, binary2) == add_binary_builtin(binary1, binary2)
 
 
 implementations = [add_binary_builtin, add_binary_boolean]
